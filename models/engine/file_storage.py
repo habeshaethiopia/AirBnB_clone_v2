4524fd66c2410d20d3c221311ba539fc3edaabb3
OmarTl2005 <hiba41145@gmail.com>
2023-09-16 19:45:23 +0100

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,15 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
