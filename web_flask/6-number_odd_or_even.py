be9f32ea991da6245b4bcd8cbb44c2f1e20e11d8
Adane <adanemoges6@gmail.com>
2023-10-22 01:43:45 +0300

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -35,9 +35,11 @@ def num(n):
 def number_template(n):
     return render_template("5-number.html", n=n)
 
+
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     return render_template("6-number_odd_or_even.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
