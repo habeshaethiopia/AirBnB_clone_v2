7d0174a9c998df2e3ac0aa86c8e32fb5f7b16c6b
Adane <adanemoges6@gmail.com>
2023-10-21 23:25:17 +0300

    pycodestyle for task 1 fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def hbnb():
     return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
