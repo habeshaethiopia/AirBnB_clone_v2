3ca4419d8e1148f36fd69717c4d274e8f3731827
Adane <adanemoges6@gmail.com>
2023-10-21 23:51:00 +0300

    task 3 done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,10 +19,12 @@ def hbnb():
 def C(text):
     return "C " + text.replace("_", " ")
 
+
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python", strict_slashes=False)
-def python(text = "is cool"):
+def python(text="is cool"):
     return "Python " + text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
