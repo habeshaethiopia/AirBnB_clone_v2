bf116d1c31356e3b2c340e84d80d86d9e565f9c1
Adane <adanemoges6@gmail.com>
2023-10-22 21:03:54 +0300

    GIT LASH

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage."""
-    states = storage.all("State")
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
