56eb973e7f273bc6e74282f3a1a16173e59d1dd9
Adane <adanemoges6@gmail.com>
2023-10-22 03:37:25 +0300

    having some bugs

--- models/state.py
+++ models/state.py
@@ -23,7 +23,9 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
-
+    __mapper_args__ = {
+        'polymorphic_identity': 'child'
+    }
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
